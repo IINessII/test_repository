d10db57a93fab0799d5376462fcbb3195f739904
NESTOR ROMERO <nestorgue08@gmail.com>
2021-11-17 17:57:48 -0500

    Test Actualizacion

--- models/servicioModelo.js
+++ models/servicioModelo.js
@@ -17,23 +17,22 @@ app.use(express.static("public"));
 const path = require("path");
 
 
- require('./conexion')
+require('./conexion')
 
 
  
      
- app.post("/upload", (req, res) => {
+app.post("/upload", (req, res) => {
   var myobj = {nombres: req.body.nombres, apellidos: req.body.apellidos, correo: req.body.correo, celular: req.body.celular, clave: req.body.clave};
   modelo.collection.insertOne(myobj, function(err, res) {
   if (err) throw err;
-
-  })
+})
   res.send("Datos insertados correctamente")
-  })
+})
 
 
-  app.get('/', (req, res) => {
-    res.writeHead(200, { 'Content-Type': HTML_CONTENT_TYPE })
+app.get('/', (req, res) => {
+  res.writeHead(200, { 'Content-Type': HTML_CONTENT_TYPE })
  
 
   createReadStream('./index.html').pipe(res)
@@ -41,8 +40,8 @@ const path = require("path");
   })
 
 
-  app.listen(600, () => {
+app.listen(600, () => {
 
-    console.log("aplicacion corriendo en el puerto 600")
+  console.log("aplicacion corriendo en el puerto 600")
     
-    })
+  })
